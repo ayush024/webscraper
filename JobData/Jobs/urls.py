c94bf28ac58fa55d6c703a2c1a8e9f841415361f
Aayush <aayushdhakal360@gmail.com>
2018-10-02 21:09:09 +0545

    buttons work

--- JobData/Jobs/urls.py
+++ JobData/Jobs/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
 	path('', views.home.as_view(), name='home'),
 	path('topjobs/', views.top.as_view(), name='top'),
 	path('topjobs/list', views.toplist.as_view(), name='toplist'),
-	path('hotjobs/', views.hot.as_view(), name='hot'),
+	path('topjobs/emptylist', views.topempty.as_view(), name='topemptylist'),
+	path('hotjobs/', views.hot.as_view(), name='hot'),	
+	path('hotjobs/list', views.hotlist.as_view(), name='hotlist'),
+	path('hotjobs/emptylist', views.hotempty.as_view(), name='hotemptylist'),
 	path('featuredjobs/', views.featured.as_view(), name='featured'),
+	path('featuredjobs/list', views.featuredlist.as_view(), name='featuredlist'),
+	path('featuredjobs/emptylist', views.featuredempty.as_view(), name='featuredemptylist'),
 ]
--- JobData/Jobs/views.py
+++ JobData/Jobs/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from .models import TopJobs, HotJobs, FeaturedJobs
 from django.views.generic import View
 from . import scraper
-
+from django.http import HttpResponse
 
 class home(View):
 	template = 'Jobs/home.html'
 	
-	def get(self, request):
-		return render(request, self.template)
-
 class top(View):
 	model = TopJobs
 	template = 'Jobs/top.html'
@@ -24,37 +21,65 @@ class top(View):
 		return render(request, self.template)
 
 class toplist(View):
-
 	model = TopJobs
 	template = 'Jobs/top.html'
 	def get(self, request):
 		return render(request, self.template, {'jobs': scraper.top_jobs, 'visibility': True})
-#		return HttpResponse('<p>Hi.</p>')
+
+class topempty(View):
+	model = TopJobs
+	template = 'Jobs/top.html'
+	def get(self, request):
+		return render(request, self.template, {'jobs': scraper.top_jobs, 'visibility': False})
 
 class hot(View):
 	model = HotJobs
 	template = 'Jobs/hot.html'
 	
 	def get(self, request):
-		return render(request, self.template)
+		return render(request, self.template, {'jobs': scraper.hot_jobs, 'visibility': False})
 
-	def post(self, request, context):
+	def post(self, request):
 		HotJobs.objects.all().delete()
 		for job in scraper.hot_jobs:
 			a=HotJobs(Company = job['company'], Position = job['position'])
 			a.save()
-		return render(request, self.template, {'jobs': scraper.hot_jobs})
+		return render(request, self.template)
+
+class hotlist(View):
+	model = HotJobs
+	template = 'Jobs/hot.html'
+	def get(self, request):
+		return render(request, self.template, {'jobs': scraper.hot_jobs, 'visibility': True})
+
+class hotempty(View):
+	model = HotJobs
+	template = 'Jobs/hot.html'
+	def get(self, request):
+		return render(request, self.template, {'jobs': scraper.hot_jobs, 'visibility': False})
 
 class featured(View):
 	model = FeaturedJobs
 	template = 'Jobs/featured.html'
 	
 	def get(self, request):
-		return render(request, self.template)
+		return render(request, self.template, {'jobs': scraper.featured_jobs, 'visibility': False})
 
-	def post(self, request, context):
+	def post(self, request):
 		FeaturedJobs.objects.all().delete()
 		for job in scraper.featured_jobs:
 			a=HotJobs(Company = job['company'], Position = job['position'])
 			a.save()
-		return render(request, self.template, {'jobs': scraper.featured_jobs})
+		return render(request, self.template)
+
+class featuredlist(View):
+	model = FeaturedJobs
+	template = 'Jobs/featured.html'
+	def get(self, request):
+		return render(request, self.template, {'jobs': scraper.featured_jobs, 'visibility': True})
+
+class featuredempty(View):
+	model = FeaturedJobs
+	template = 'Jobs/featured.html'
+	def get(self, request):
+		return render(request, self.template, {'jobs': scraper.featured_jobs, 'visibility': False})
